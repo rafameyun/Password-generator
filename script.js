427a0fd0d6fb26dadf23e94a96f5b381eda4faec
rafameyun <raffinhah4@gmail.com>
2021-11-22 03:13:33 -0300

    Add const = getElement and function get()

---
+++ script.js
@@ -0,0 +1,29 @@
+const pallet = document.getElementById("pallet");
+const  copyEl = document.getElementById("copy");
+const lenEl = document.getElementById("len");
+const upperEl = document.getElementById("upper");
+const lowerEL = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
+
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_-=+.";
+
+function getLowercase() {
+    return lowerLetters[Math.floor(Math.randon() * lowerLetters.length)];
+}
+
+function getUppercase() {
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+
+function getNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getSymbol() {
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
